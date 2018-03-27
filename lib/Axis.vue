29f7578dc68e2a8eb9ed6f7d8cdc1d33406c8962
PJ Trainor <trainorpj@gmail.com>
2018-03-26 21:26:09 -0400

    changed to renderless components

--- lib/Axis.vue
+++ lib/Axis.vue
@@ -1,36 +1,41 @@
-<template>
-  <g>
-    <slot name="axis-ticks" v-for="t in axisTicks" v-bind="t">
-    </slot>
-  </g>
-</template>
-
-
 <script>
-import Vue from "vue"
-import { Component, Prop } from "vue-property-decorator"
-
 import { makeArrayFromTicks } from "./utils"
 
-@Component({})
-export default class Axis extends Vue {
-  @Prop() scale
-  @Prop() ticks
-  @Prop({ default: 0, type: Number })
-  top
-  @Prop({ default: 0, type: Number })
-  left
+export default {
+  props: {
+    scale: Function,
+    ticks: [Number, Array],
+    top: {
+      default: 0,
+      type: Number
+    },
+    left: {
+      default: 0,
+      type: Number
+    }
+  },
+  computed: {
+    axisTicks() {
+      const tickCount = makeArrayFromTicks({
+        scale: this.scale,
+        ticks: this.ticks
+      })
 
-  get axisTicks() {
-    const tickCount = makeArrayFromTicks({
-      scale: this.scale,
-      ticks: this.ticks
+      return tickCount.map((d, i) => ({
+        key: i,
+        pos: this.scale(d),
+        val: d
+      }))
+    }
+  },
+  render() {
+    return this.$scopedSlots.default({
+      ticks: this.axisTicks
     })
-
-    return tickCount.map((d, i) => ({
-      pos: this.scale(d),
-      val: d
-    }))
   }
 }
 </script>
+
+<style>
+
+</style>
